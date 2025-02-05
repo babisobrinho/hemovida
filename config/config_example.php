8903c568f08b220a64558f889ab407950d699c9f
Babi Sobrinho <79843387+babisobrinho@users.noreply.github.com>
2025-02-05 23:31:39 +0000

    create gitignore

---
+++ config/config_example.php
@@ -0,0 +1,21 @@
+<?php
+
+    header('Content-Type: text/html; charset=utf-8');
+
+    // Configuração da base de dados
+    define('DB_HOST', '');
+    define('DB_NAME', '');
+    define('DB_USER', '');
+    define('DB_PASS', '');
+    define('DB_CHARSET', 'utf8');
+
+    // Variáveis globais
+    define('BASE_URL', 'http://localhost/');
+
+    // Definir o fuso horário
+    date_default_timezone_set('Europe/Lisbon');
+
+    // Configurações de segurança (ex. chave secreta para sessões, tokens CSRF, etc.)
+    define('SECRET_KEY', '');
+
+?>
